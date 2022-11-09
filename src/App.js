2314600314bac121260d247b7542ddd3b0a11f2c
lecuong220120 <lecuong220120@gmail.com>
2022-11-09 17:09:52 +0700

    fisrt code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
-
+import {Home, SingleProduct, Cart, Checkout, Error, About, Products, PrivateRoute} from './pages'
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return(
+ <Router>
+  <Navbar/>
+  <Sidebar/>
+    <Switch>
+      <Route exact path='/'>
+        <Home/>
+      </Route>
+      <Route exact path='/about'>
+        <About/>
+      </Route>
+      <Route exact path='/cart'>
+        <Cart/>
+      </Route>
+      <Route exact path='/products'>
+        <Products/>
+      </Route>
+      <Route exact path='/product/:id' children = {<SingleProduct/>}></Route>
+      <Route exact path='/checkout'>
+        <Checkout/>
+      </Route>
+      <Route path="*">
+        <Error/>
+      </Route>
+    </Switch>
+  <Footer/>
+ </Router>
+  )
 }
 
 export default App
